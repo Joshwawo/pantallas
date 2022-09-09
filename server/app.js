d8806973889662678df35f16d7a052700d9df0c5
Joshwawo <masacota_masacota@hotmail.com>
2022-09-08 20:22:37 -0700

    fix:erros on render

--- server/app.js
+++ server/app.js
@@ -1,29 +1,27 @@
 import express from "express";
-import fileupload from 'express-fileupload'
+import fileupload from "express-fileupload";
 import postRoutes from "./routes/post.routes.js";
-import cors from 'cors'
-import {dirname, join} from "path";
-import {fileURLToPath} from 'url'
-
+import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
-
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Middlewares
-app.use(cors())
-app.use(express.json())
-app.use(fileupload({
-    useTempFiles:true,
-    tempFileDir: './upload',
+app.use(cors());
+app.use(express.json());
+app.use(
+  fileupload({
+    useTempFiles: true,
+    tempFileDir: "./upload",
     // tempFileDir: './upload/temp'
-}))
+  })
+);
 
 //Rutas
 app.use(postRoutes);
-
 
 // console.log(__dirname)
 // app.use(express.static(join(__dirname, '../client/build')))
@@ -32,5 +30,4 @@ app.use(postRoutes);
 //         res.sendFile(join(__dirname,'../sistemawebpantallas/build/index.html'))
 // })
 
-
-export default app
+export default app;
